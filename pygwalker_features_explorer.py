64f9a5c980b97cb292a3d1ae39e621350f7e130e
Quentin Villotta <quentin.villotta@gmail.com>
2024-05-09 16:49:38 +0300

    Try deploy streamlit V simplified

--- pygwalker_features_explorer.py
+++ pygwalker_features_explorer.py
@@ -7,10 +7,13 @@ st.set_page_config(
     page_title="Use Pygwalker In Streamlit",
     layout="wide"
 )
-# Import Features data
-features_path = "data/HSOS_RD09/model_input/features.xlsx"
-features = pd.read_excel(features_path)
 
-# Visualizer
-pyg_app = StreamlitRenderer(features)
-pyg_app.explorer()
+
+st.write(":balloon:")
+# Import Features data
+# features_path = "data/HSOS_RD09/model_input/features.xlsx"
+# features = pd.read_excel(features_path)
+
+# # Visualizer
+# pyg_app = StreamlitRenderer(features)
+# pyg_app.explorer()
