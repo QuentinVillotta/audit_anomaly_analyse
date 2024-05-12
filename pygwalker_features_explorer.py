326a05ef15828653355baefabc04a74cb456ec54
Quentin Villotta <quentin.villotta@gmail.com>
2024-05-12 11:55:47 +0300

    Try deploy streamlit V simplified 0.5 - FIle Uploader feature

--- pygwalker_features_explorer.py
+++ pygwalker_features_explorer.py
@@ -1,7 +1,22 @@
 from pygwalker.api.streamlit import StreamlitRenderer
 import pandas as pd
 import streamlit as st
- 
+import base64
+import json
+import pickle
+import uuid
+import re
+
+def set_clicked():
+    st.session_state.clicked = True
+    
+# You should cache your pygwalker renderer, if you don't want your memory to explode
+@st.cache_resource
+def get_pyg_renderer(df) -> "StreamlitRenderer":
+    # If you want to use feature of saving chart config, set `spec_io_mode="rw"`
+    return StreamlitRenderer(df, spec="./gw_config.json", spec_io_mode="rw")
+
+
 # Adjust the width of the Streamlit page
 st.set_page_config(
     page_title="Features Explorer",
@@ -11,15 +26,17 @@ st.set_page_config(
 # Title
 st.title("Features Explorer")
 
-# You should cache your pygwalker renderer, if you don't want your memory to explode
-@st.cache_resource
-def get_pyg_renderer() -> "StreamlitRenderer":
-    # Import Features data
-    features_path = "app_data/features.xlsx"
-    features = pd.read_excel(features_path)
-    # If you want to use feature of saving chart config, set `spec_io_mode="rw"`
-    return StreamlitRenderer(features, spec="./gw_config.json", spec_io_mode="rw")
 
-renderer = get_pyg_renderer()
+if 'clicked' not in st.session_state:
+    st.session_state.clicked = False
 
-renderer.explorer()
+st.button('Upload File', on_click=set_clicked)
+if st.session_state.clicked:
+    uploaded_file = st.file_uploader("Choose a file")
+    if uploaded_file is  None:
+        st.write("Please upload a dataset")
+    else:
+        st.write("You selected the file:", uploaded_file.name)
+        data = pd.read_excel(uploaded_file)
+        renderer = get_pyg_renderer(df=data)
+        renderer.explorer()
